34c4d77f798ad53c705231d972383ab48fce3f0e
scvalencia <sc.valencia606@uniandes.edu.co>
2017-04-19 17:29:16 -0500

    Assets and images for status

---
+++ seneca/ConnectionStatusView.swift
@@ -0,0 +1,19 @@
+//
+//  ConnectionStatusView.swift
+//  seneca
+//
+//  Created by Sebastian Camilo Valencia on 4/19/17.
+//  Copyright © 2017 Sebastian Camilo Valencia. All rights reserved.
+//
+
+import Cocoa
+
+class ConnectionStatusView: NSView {
+
+    override func draw(_ dirtyRect: NSRect) {
+        super.draw(dirtyRect)
+
+        // Drawing code here.
+    }
+    
+}
