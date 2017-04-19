d2a824ab831dfa0c037f46ea5e880948c3a2b07d
scvalencia <sc.valencia606@uniandes.edu.co>
2017-04-18 22:53:49 -0500

    Adding dummy documentation on the logic of the app: AboutWindow.swift

--- seneca/AboutWindow.swift
+++ seneca/AboutWindow.swift
@@ -14,8 +14,15 @@ let NAME_LABEL_TEXT = "seneca"
 
 class AboutWindow: NSWindowController {
 
+    /*
+     * The UI components related to this particular view, specified 
+     * to handle them in this controller
+    */
+
     @IBOutlet weak var logoIcon: NSImageView!
+
     @IBOutlet weak var nameLabel: NSTextField!
+    
     @IBOutlet weak var versionLabel: NSTextField!
     
     override var windowNibName : String! {
@@ -29,11 +36,14 @@ class AboutWindow: NSWindowController {
         self.window?.makeKeyAndOrderFront(nil)
         NSApp.activate(ignoringOtherApps: true)
         
+        // Handling the logo image
         self.logoIcon.image = NSImage(named: "logo")
         
+        // Handling the project name label
         self.nameLabel.font = NSFont(name: "Helvetica Neue Light", size: 14)
         self.nameLabel.font = NSFont.boldSystemFont(ofSize: 14.0)
         
+        // Handling the project version label
         self.versionLabel.stringValue = "Version \(APP_VERSION)"
         self.versionLabel.font = NSFont(name: "Helvetica Neue Light", size: 10)
     }
