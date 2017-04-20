c8514034ba3aa4861f56e139efbaa03945baf45c
scvalencia <sc.valencia606@uniandes.edu.co>
2017-04-20 02:03:33 -0500

    Adding a service to list reacheable WiFi networks

--- seneca/SENECALoginController.swift
+++ seneca/SENECALoginController.swift
@@ -7,13 +7,43 @@
 //
 
 import Foundation
+import CoreWLAN
 
 class SENECALoginController {
     
     let BASE_URL = "https://wlan.uniandes.edu.co/login.html"
     
     func doConnectionPOST() {
-        
+    }
+    
+    
+}
+
+class Discovery {
+    
+    var currentInterface: CWInterface
+    var interfacesNames: [String] = []
+    var networks: Set<CWNetwork> = []
+    
+    // Failable init using default interface. Fails if no connection detected
+    init?() {
+        if let defaultInterface = CWWiFiClient.shared().interface(),
+            let name = defaultInterface.interfaceName {
+            self.currentInterface = defaultInterface
+            self.interfacesNames.append(name)
+            self.findNetworks()
+        } else {
+            return nil
+        }
+    }
+    
+    // Fetch detectable WIFI networks
+    func findNetworks() {
+        do {
+            self.networks = try currentInterface.scanForNetworks(withSSID: nil)
+        } catch let error as NSError {
+            print("Error: \(error.localizedDescription)")
+        }
     }
     
 }
