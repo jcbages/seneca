a8136cf8baebc7cadbb394a0f970d54523883e4c
Juan Camilo Bages <jcbages@outlook.com>
2017-04-22 10:44:07 -0500

    Added generic .gitignore

---
+++ seneca/SenecaLoginController.swift
@@ -0,0 +1,22 @@
+import Foundation
+import CoreWLAN
+
+class SENECALoginController {
+    
+    let BASE_URL = "https://wlan.uniandes.edu.co/login.html"
+    
+    let WIFI_NETWORK_NAME = "SENECA"
+    
+    func doConnectionPOST() {}
+    
+    func isSenecaCurrentSSID() -> Bool {
+        if let wifiInterface = CWWiFiClient.shared().interface(), let ssid = wifiInterface.ssid() {
+            return ssid == WIFI_NETWORK_NAME
+        }
+        return false;
+    }
+    
+    func connectedToSeneca() {}
+    
+    func connectedToInternet() {}
+}
